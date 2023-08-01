9fe32cbaf0a3bec0227b6a5fe4b371772adaca52
Phung Duc Dung <ducdungphung2002@gmail.com>
2023-08-01 15:12:54 +0700

    Login and Sign Up

--- src/components/LoginSignup/SignUp.js
+++ src/components/LoginSignup/SignUp.js
@@ -1,11 +1,53 @@
-import React from 'react';
-
+import React from "react";
+import Form from 'react-bootstrap/Form';
 const SignUp = () => {
-    return (
-        <>
-         Sign Up   
-        </>
-    );
+  return (
+    <>
+      <div class="auth-page">
+        <div class="container page">
+          <div class="row">
+            <div class="col-md-6 offset-md-3 col-xs-12 text-center">
+              <h1 class="text-xs-center">Sign up</h1>
+              <p class="text-xs-center">
+                <a href="/login" style={{color: 'green '}}>Have an account?</a>
+              </p>
+
+              {/* <ul class="error-messages">
+                <li>That email is already taken</li>
+              </ul> */}
+
+              <Form>
+                <fieldset class="form-group">
+                  <input
+                    class="form-control form-control-lg m-2"
+                    type="text"
+                    placeholder="Username"
+                  />
+                </fieldset>
+                <fieldset class="form-group">
+                  <input
+                    class="form-control form-control-lg m-2"
+                    type="text"
+                    placeholder="Email"
+                  />
+                </fieldset>
+                <fieldset class="form-group">
+                  <input
+                    class="form-control form-control-lg m-2"
+                    type="password"
+                    placeholder="Password"
+                  />
+                </fieldset>
+                <button class="btn btn-lg btn-success pull-xs-right" >
+                  Sign up
+                </button>
+              </Form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default SignUp;

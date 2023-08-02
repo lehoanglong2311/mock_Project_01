280102c6faf662aa94e9748a95d90b53b1b6037e
lehoanglong2311 <lehoanglong2311@gmail.com>
2023-08-02 10:40:36 +0700

    luu tam

--- src/components/Articles/Home.js
+++ src/components/Articles/Home.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import { Col } from 'react-bootstrap';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { useNavigate, Outlet, NavLink } from 'react-router-dom';
+
 const Home = () => {
     return (
 
@@ -10,12 +14,29 @@ const Home = () => {
                 <h1 className='mb-3'>conduit</h1>
                 <h5 className='mb-3'>A place to share your knowledge.</h5>
             </div>
-            <Container className='mt-5'>
+            {/* <Container className='mt-5'>
                 <Row>
-                    <Col sm={{ size: 6, order: 1, offset: 1 }}>Global Feed</Col>
+                    <Col sm={{ size: 6, order: 1, offset: 4 }}>Global Feed</Col>
                     <Col sm={{ size: 3, offset: 2, order: 2 }}>Popular Tags</Col>
                 </Row>
-            </Container>
+            </Container> */}
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-10">
+                        <Navbar  >
+                            <Container>
+                                <Nav className="me-auto">
+                                    <NavLink  className="nav-link" to="/" >Global Feed</NavLink>
+                                    <NavLink className="nav-link" to="/">Your Feed</NavLink>
+                                </Nav>
+                            </Container>
+                        </Navbar>
+                    </div>
+
+                    <div className="col-md-2"> Popular Tags</div>
+                </div>
+            </div>
         </>
     );
 };
